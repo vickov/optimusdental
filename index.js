64dfc99a049e72f3e2a35bed7b67f8b8ba0ba1a6
Vicko <vicko@diagram.hr>
2020-02-09 11:02:45 +0100

    optimudental

--- index.js
+++
@@ -1,9 +0,0 @@
-var	app = require('express')(),
-	port = process.env.PORT || 8080;
-
-app.use(require('compression')());
-app.use(require('serve-static')('public/'));
-
-app.listen(port, function() {
-	console.log('Server listening on localhost:'+port);
-});
